032639371e1d669baabb3c60078030a4a9cbf00d
Lukasz Tekieli <tekieli.lukasz@gmail.com>
2023-01-04 21:41:54 +0100

    Add docker file for build environment

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(realpath $(dirname "$0"))
+
+docker_build() {
+    docker build -t oe-env \
+        --build-arg USER="${USER}" \
+        --build-arg USERID="$(id -u)" \
+        --build-arg GROUPID="$(id -g)" \
+        ${SCRIPT_DIR}/docker
+}
+
+docker_run() {
+    docker run --rm -it -u $(id -u):$(id -g) --network=host \
+        -v "$HOME"/.ssh:"${HOME}"/.ssh \
+        -v ${SCRIPT_DIR}:/workspace \
+        -v /opt/oe:/opt/oe \
+        oe-env /bin/bash
+}
+
 build() {
     BOARD="$1"
     kas shell "kas-${BOARD}-project.yml" -c "bitbake core-image-minimal -c clean"
@@ -9,6 +27,10 @@ build() {
     kas shell "kas-${BOARD}-project.yml" -c "bitbake package-index"
 }
 
-build bbb
-build rpi0w
-build x86-64
+if [ -z "$1" ]; then
+    build bbb
+    build rpi0w
+    build x86-64
+else
+    "$@"
+fi
